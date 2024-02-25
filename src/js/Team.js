c9ae0dd5c0befa2f877dbefd26b1daede44d4e03
Морозова Наталия <ns_morozova@mail.ru>
2024-02-25 21:48:27 +0300

    Revised project

--- src/js/Team.js
+++ src/js/Team.js
@@ -8,21 +8,21 @@ export default class Team {
           new Character('Волшебник', 'Magician', 50, 1, 40, 10),
           new Character('Демон', 'Daemon', 50, 1, 40, 10),
         ]);
+      }
 
-        [Symbol.iterator] = function() {
-            const charList = Array.from(this.members);
-            let current = 0;
-            const last = charList.length - 1;
-        
-            return {
-              next() {
-                if (current <= last) {
-                  return { done: false, value: charList[current++] };
-                }
-                return { done: true };
-              },
-            };
-          }
+      [Symbol.iterator] = function() {
+        const charList = Array.from(this.members);
+        let current = 0;
+        const last = charList.length - 1;
+    
+        return {
+          next() {
+            if (current <= last) {
+              return { done: false, value: charList[current++] };
+            }
+            return { done: true };
+          },
+        };
       }
       
 }
